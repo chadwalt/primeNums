989d8f76115336baa01bf6205861c8ac09729b41
chadwalt <chadwalt@outlook.com>
2017-07-03 17:32:40 +0300

    Adding the float test function

--- primeNumbers.py
+++ primeNumbers.py
@@ -4,21 +4,24 @@ import unittest ## Inport the pytest for testing.
 
 def get_user_input ():
     user_input = input('Enter a number')
-    return type (user_input)
+    return user_input
 
 class Tests (unittest.TestCase) :       
     ## User input cannot be a string.
-    def string_test (self):
-        assert get_user_input() == 'int'
+    def test_string_test (self):
+        assert type(get_user_input() )== 'int'
 
     ## User input cannot be a negative. 
-    def negative_test ():
-        assert get_user_input() < 0
+    def test_negative_test (self):
+        assert int(get_user_input()) < 0
 
     ## User input cannot be one.
-    def one_test ():
-        assert get_user_input () == 1
+    def test_one_test (self):
+        assert int(get_user_input ()) == 1
 
+    ## User input is not a floating point number.
+    def test_float_num (self) :
+        assert type(get_user_input()) == 'float'
 
 if __name__ == '__main__':
     unittest.main()
