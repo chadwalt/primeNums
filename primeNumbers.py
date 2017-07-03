5bab46a2382f242d81192ddc08b6d7a09a1de167
chadwalt <chadwalt@outlook.com>
2017-07-03 17:53:12 +0300

    Adding the empty string test

--- primeNumbers.py
+++ primeNumbers.py
@@ -9,19 +9,23 @@ def get_user_input ():
 class Tests (unittest.TestCase) :       
     ## User input cannot be a string.
     def test_string_test (self):
-        assert type(get_user_input() )== 'int'
+        self.assertEqual(type(get_user_input()),  'int')
 
     ## User input cannot be a negative. 
     def test_negative_test (self):
-        assert int(get_user_input()) < 0
+        self.assert_(int(get_user_input()) < 0)
+
+    ## User input cannot be an empty string.
+    def test_empty_string (self):
+        self.assertEqual(get_user_input(), '')
 
     ## User input cannot be one.
     def test_one_test (self):
-        assert int(get_user_input ()) == 1
+        self.assertEqual(int(get_user_input ()) ,1)
 
     ## User input is not a floating point number.
     def test_float_num (self) :
-        assert type(get_user_input()) == 'float'
+        self.assertEqual(type(get_user_input()), 'float')
 
 if __name__ == '__main__':
     unittest.main()
