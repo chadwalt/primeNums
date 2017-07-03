1e99ef2cfd7bcd79a072def1f778af25b6ebee65
chadwalt <chadwalt@outlook.com>
2017-07-03 17:14:32 +0300

    Creating  a class for the tests

--- primeNumbers.py
+++ primeNumbers.py
@@ -6,18 +6,18 @@ def get_user_input ():
     user_input = input('Enter a number')
     return type (user_input)
 
-## User input cannot be a string.
-def string_test ():
-    assert get_user_input() == 'int'
+class Tests (unittest.TestCase) :       
+    ## User input cannot be a string.
+    def string_test (self):
+        assert get_user_input() == 'int'
 
-## User input cannot be a negative. 
-def negative_test ():
-    assert get_user_input() < 0
+    ## User input cannot be a negative. 
+    def negative_test ():
+        assert get_user_input() < 0
 
-## User input cannot be one.
-
-def one_test ():
-    assert get_user_input () == 1
+    ## User input cannot be one.
+    def one_test ():
+        assert get_user_input () == 1
 
 
 if __name__ == '__main__':
